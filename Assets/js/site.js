334426387182c444f3b3bb0f54bf4a247276ea9b
kuvondik <quvondiqbek@outlook.com>
2021-01-20 02:11:15 +0500

    hot fixes

--- Assets/js/site.js
+++ Assets/js/site.js
@@ -11,6 +11,22 @@ var data = [
             id: 1,
             name: "Child1_1",
             price: "price1",
+            children: [
+              {
+                id: 1,
+                name: "Child1_1",
+                price: "price1",
+              },
+              {
+                id: 1,
+                name: "Child1_2",
+                price: "price2",
+              },
+              {
+                id: 1,
+                name: "Child1_3",
+                price: "price3",
+              },]
           },
           {
             id: 1,
@@ -27,27 +43,7 @@ var data = [
       {
         id: 2,
         name: "Child2",
-        children: [
-          {
-            id: 1,
-            name: "Child2_1",
-            price: "price1",
-          },
-          {
-            id: 1,
-            name: "Child2_2",
-            price: "price2",
-          },
-          {
-            id: 1,
-            name: "Child2_3",
-            price: "price3",
-          },
-        ],
-      },
-      {
-        id: 3,
-        name: "Child3",
+        children: [],
       },
     ],
   },
@@ -58,6 +54,23 @@ var data = [
   {
     id: "3",
     name: "Name3",
+    children: [
+      {
+        id: 1,
+        name: "Child1",
+        price: "price1",
+      },
+      {
+        id: 2,
+        name: "Child2",
+        price: "price2",
+      },
+      {
+        id: 3,
+        name: "Child3",
+        price: "price3",
+      },
+    ],
   },
 ];
 
@@ -69,7 +82,22 @@ $(document).ready(function () {
     //editing:true,
     data: data,
     rowRenderer: function (item, itemIndex) {
-        return createRows(item, 'children');
+      return createRows(item, "children");
+      // var $tr1 = $('<tr class="jsgrid-row">');
+      // var $td11 = $('<td class="jsgrid-cell">');
+      // var $td12 = $('<td class="jsgrid-cell">');
+      // $tr1.append($td11);
+      // $tr1.append($td12);
+
+      // var $tr2 = $('<tr class="jsgrid-row">');
+      // var $td21 = $('<td class="jsgrid-cell">');
+      // var $td22 = $('<td class="jsgrid-cell">');
+      // $tr2.append($td21);
+      // $tr2.append($td22);
+
+      // $tr1 = $tr1.add($tr2);
+
+      // return $tr1;
     },
     // editRowRenderer: function(item, itemIndex) {
     //     var $tr = $('<tr class="jsgrid-row text-danger">');
@@ -94,6 +122,10 @@ $(document).ready(function () {
 function createRows(element, propName, level = 0) {
   // Ancestor
   var $tr = $('<tr class="jsgrid-row">');
+  $tr.attr('data-level',level);
+
+  if(level > 0)
+    $tr.addClass('collapse');
 
   //Append expander for the first column
   var $expander = $('<img class="expander">').attr(
@@ -103,38 +135,65 @@ function createRows(element, propName, level = 0) {
 
   // Child rows
   if (element[propName] && element[propName].length > 0) {
-    // Columns
+    // Column name
     var $tdName = $('<td class="jsgrid-cell">');
-    
+
     // Add space blocks
-    var $spaceBlock = $('<span class="space-block">');
-    for(var i=0; i < level; i++){
-        $tdName.append($spaceBlock);
+    for (var i = 0; i < level; i++) {
+      var $spaceBlock = $('<span class="space-block">');
+      $tdName.append($spaceBlock);
     }
-    
+
     level++;
-    
+
     $tdName.append($expander);
     $tdName.append($("<span>").append(element.name));
 
+    // Column price
     var $tdPrice = $('<td class="jsgrid-cell">');
     $tdPrice.append($("<span>").append(element.price));
 
     $tr.append($tdName).append($tdPrice);
 
+    var $childRows;
     for (var i = 0; i < element[propName].length; i++) {
-      $tr.append(createRows(element[propName][i], propName, level));
+      $childRows = createRows(element[propName][i], propName, level);
+      $tr = $tr.add($childRows);
     }
 
+    $expander.click(function(){
+      if($(this).hasClass('expanded')){
+        $(this).removeClass('expanded');
+        for(var i=1; i < $tr.length; i++){
+          $($tr[i]).addClass('collapse');
+          $($tr[i]).find('.expander').removeClass('expanded');
+        }  
+      }
+      else{
+        $(this).addClass('expanded');
+        for(var i=1; i < $tr.length; i++){
+          if(level == $($tr[i]).attr('data-level'))
+            $($tr[i]).removeClass('collapse');
+        }
+      }
+    });
   } else {
     // Hide expander for rows without children
-    $expander.css('visibility', 'hidden');
+    $expander.css("visibility", "hidden");
 
-    // Columns
+    // Column name
     var $tdName = $('<td class="jsgrid-cell">');
+
+    // Add space blocks
+    for (var i = 0; i < level; i++) {
+      var $spaceBlock = $('<span class="space-block">');
+      $tdName.append($spaceBlock);
+    }
+
     $tdName.append($expander);
     $tdName.append($("<span>").append(element.name));
 
+    // Column price
     var $tdPrice = $('<td class="jsgrid-cell">');
     $tdPrice.append($("<span>").append(element.price));
 
